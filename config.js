76c03e92a3fbffb9bb748f1461d902e40447f4ab
camphanho <32627783+camphanho@users.noreply.github.com>
2022-02-03 19:33:12 +0700

    update

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
     titleWeb: "Thương em ^^",
     introTitle: 'Thường Phan',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `<p>Trái đất vốn lạ thường</p>
+    <p>Mà sao em cứ đi nhầm đường</p>
+    <p>Lạc vào tim anh lẻ loi</p>
+    <p>Đằng sau chữ yêu đây là thương</p>`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
