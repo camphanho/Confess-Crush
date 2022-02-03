f23b36e3b15ba5737d2f3f60daca95e041dd2def
camphanho <32627783+camphanho@users.noreply.github.com>
2022-02-03 19:37:00 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 function init() {
     document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb
     $('#title').text(CONFIG.title)
-    $('#desc').html(CONFIG.desc)
+    $('#desc').append(CONFIG.desc)
     $('#yes').text(CONFIG.btnYes)
     $('#no').text(CONFIG.btnNo)
 
